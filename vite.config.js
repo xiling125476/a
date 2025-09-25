b8225dc147b45c5610e438c7c3e493a7e0d46e1d
xiling125476 <clingja@gmail.com>
2025-09-25 11:49:02 +0700

    sa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github.io/a/'
+  base: '/xiling125476.github.io/a/'
 })
